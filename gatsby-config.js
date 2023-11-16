91cce12788e11479bfd221fbd5225d8b4a39192c
vishu5683 <vishubhardwaj46@gmail.com>
2023-11-16 16:19:00 +0530

    Your commit message here

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,40 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
+  plugins: [
+     // Your other plugins...
+     {  
+     resolve: `gatsby-plugin-mdx`,
+     options: {
+       extensions: [`.mdx`, `.md`],
+     },
+   },
+     {
+       resolve: `gatsby-source-filesystem`,
+       options: {
+         name: `pages`,
+         path: `${__dirname}/src/pages`,
+       },
+     },
+     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `components`,
+        path: `${__dirname}/src/components`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+        ],
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,58 @@
-import * as React from "react"
+import React from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import { MDXProvider } from '@mdx-js/react';
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const MyH1 = props => <h1 style={{ color: 'tomato' }} {...props} />;
+const MyParagraph = props => (
+  <p style={{ fontSize: '18px', lineHeight: 1.6 }} {...props} />
+);
+
+const components = {
+  h1: MyH1,
+  p: MyParagraph,
+};
+
+const DemoPage = ({ data }) => {
+  const { allMdx } = data;
+
+  return (
+    <Layout>
+      <h1>All MDX Frontmatter</h1>
+      <MDXProvider components={components}>
+        <div>
+          {allMdx.nodes.map(({ frontmatter, body }, index) => (
+            <div key={index}>
+              <strong>Title:</strong> {frontmatter.title},{' '}
+              <strong>Date:</strong> {frontmatter.date},{' '}
+              <strong>Slug:</strong> {frontmatter.slug}
+           
+              {body}
+            </div>
+          ))}
+        </div>
+      </MDXProvider>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query MyQuery {
+    allMdx {
+      nodes {
+        frontmatter {
+          ...MdxFrontmatter
+        }
+        body
+      }
+    }
+  }
+
+  fragment MdxFrontmatter on MdxFrontmatter {
+    date
+    slug
+    title
+  }
+`;
+
+export default DemoPage;
